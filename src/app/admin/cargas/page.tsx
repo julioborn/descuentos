d36c7a88708c26b4df6108362b8821ef0a1d2059
julioborn <juliobornes10@gmail.com>
2025-08-16 18:13:09 -0300

    solucion error minimo

--- src/app/admin/cargas/page.tsx
+++ src/app/admin/cargas/page.tsx
@@ -934,7 +934,7 @@ export default function CargasPage() {
 
                         {/* Botones editar/eliminar */}
                         <div className="flex justify-end gap-3">
-                            {/* <button
+                            <button
                                 onClick={() => editarCarga(c._id)}
                                 className="px-4 py-1 text-sm bg-yellow-600 hover:bg-yellow-500 rounded-full"
                             >
@@ -942,7 +942,7 @@ export default function CargasPage() {
                                     <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
                                     <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
                                 </svg>
-                            </button> */}
+                            </button>
                             <button
                                 onClick={() => eliminarCarga(c._id)}
                                 className="px-4 py-1 text-sm bg-red-700 hover:bg-red-600 rounded-full"
@@ -956,7 +956,7 @@ export default function CargasPage() {
                 ))}
             </div>
 
-            {/* -------- Paginación -------- */}a
+            {/* -------- Paginación -------- */}
             {totalPag > 1 && (
                 <div className="flex justify-center items-center gap-2 mt-8 flex-wrap">
                     {/* Botón Anterior */}
